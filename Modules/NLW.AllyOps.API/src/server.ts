35edaec52946d53615acb06979dd9f371b9ea184
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-09-11 21:23:02 -0300

    Finalização das configurações básicas da BASE API

--- Modules/NLW.AllyOps.API/src/server.ts
+++ Modules/NLW.AllyOps.API/src/server.ts
@@ -7,6 +7,7 @@ app.get("/ads", (request, response) => {
     { id: 1, nome: "Anuncio 1" },
     { id: 2, nome: "Anuncio 2" },
     { id: 3, nome: "Anuncio 3" },
+    { id: 4, nome: "Anuncio 4" },
   ]);
 });
 
