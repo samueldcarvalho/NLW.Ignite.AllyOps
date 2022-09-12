61ce40a289885b0e32226aecfdbebdbcf351e30d
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-09-11 22:34:12 -0300

    Criação da primeira aplicação Mobile

--- Modules/mobile/App.tsx
+++ Modules/mobile/App.tsx
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Hello Native!</Text>
+      <Button title="Opa!" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "black",
+    fontSize: 28,
+    textAlign: "center",
   },
 });
